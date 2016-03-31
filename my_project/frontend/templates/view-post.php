8b5b3df4c1afe3a3a237f64563217cb1917f3910
Anthony <waichaio973@gmail.com>
2016-03-31 14:07:19 +1300

    Delete view-post.php

--- my_project/frontend/templates/view-post.php
+++
@@ -1,7 +0,0 @@
-<a href="<?php echo $this->base->url.$_SERVER['PHP_SELF']; ?>" class="btn btn-primary">
-Return to Post List</a>
-<?php foreach ($posts as $post): ?>
- <h3>Post #<?php echo htmlspecialchars($post['id']); ?></h3>
- <?php echo htmlspecialchars($post['content']); ?>
- <hr/>
-<?php endforeach; ?>
