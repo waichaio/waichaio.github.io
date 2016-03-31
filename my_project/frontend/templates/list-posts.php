fe18d1ccfab5f56630860537bae2fa35340dda53
Anthony <waichaio973@gmail.com>
2016-03-31 14:07:09 +1300

    Delete list-posts.php

--- my_project/frontend/templates/list-posts.php
+++
@@ -1,8 +0,0 @@
-<?php foreach ($posts as $post): ?>
- <h3>Post #<?php echo htmlspecialchars($post['id']); ?></h3>
- <p><?php echo implode(' ', array_slice(explode(' ',
-strip_tags($post['content'])), 0, 10)); ?> [...]</p>
- <a href="<?php echo $this->base->url.$_SERVER['PHP_SELF']."?id=".$post['id'];
- ?>" class="btn btn-primary">Read More</a>
- <hr/>
-<?php endforeach; ?>
